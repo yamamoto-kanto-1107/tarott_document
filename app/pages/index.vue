4146738f14c1cdece29be513249de6d47ea22b81
shim37a <factory.of.horai@gmail.com>
2024-12-02 12:39:06 +0900

    nuxt/image追加

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,7 @@
   <v-container fluid class="fill-height d-flex flex-column align-center justify-center">
     <v-card width="320" elevation="0">
       <v-card-text class="d-flex flex-column align-center">
+        <nuxt-img fromat="webp" src="/images/cat.webp" width="128"/>
         <h2>Hello, Nuxt3 app.</h2>
       </v-card-text>
     </v-card>
